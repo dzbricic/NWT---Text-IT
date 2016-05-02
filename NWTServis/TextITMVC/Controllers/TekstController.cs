66a0ff11a5233a6d601356e34ebf2bc5c20a3f6b
Larisa Besic <lbesic1@etf.unsa.ba>
2016-05-02 23:49:16 +0200

    Tekst
    Tekst view

---
+++ NWTServis/TextITMVC/Controllers/TekstController.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TextITMVC.Models;
+
+namespace TextITMVC.Controllers
+{
+    public class TekstController : Controller
+    {
+
+
+
+        HttpClient client;
+        //The URL of the WEB API Service
+        string url = "http://localhost:3106/api/tekst";
+
+        //The HttpClient Class, this will be used for performing 
+        //HTTP Operations, GET, POST, PUT, DELETE
+        //Set the base address and the Header Formatter
+        public TekstController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: EmployeeInfo
+        public async Task<ActionResult> Index()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Employees = JsonConvert.DeserializeObject<List<Tekst>>(responseData);
+
+                return View(Employees);
+            }
+            return View("Error");
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Tekst());
+        }
+
+        //The Post method
+        [HttpPost]
+        public async Task<ActionResult> Create(Tekst Emp)
+        {
+
+            HttpResponseMessage responseMessage = await client.PostAsJsonAsync(url, Emp);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Employee = JsonConvert.DeserializeObject<Tekst>(responseData);
+
+                return View(Employee);
+            }
+            return View("Error");
+        }
+
+        //The PUT Method
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, Tekst Emp)
+        {
+
+            HttpResponseMessage responseMessage = await client.PutAsJsonAsync(url + "/" + id, Emp);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Employee = JsonConvert.DeserializeObject<Tekst>(responseData);
+
+                return View(Employee);
+            }
+            return View("Error");
+        }
+
+        //The DELETE method
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id, Tekst Emp)
+        {
+
+            HttpResponseMessage responseMessage = await client.DeleteAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+
+
+
+
+    }
+}
