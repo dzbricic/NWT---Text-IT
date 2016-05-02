3095e4af86c87a4e306c6a971bce1edafc87c563
Saudin Ademovic <saudin.12@hotmail.com>
2016-05-02 22:11:46 +0200

    Korisnik
    odvojeno

---
+++ NWTServis/TextITMVC/Controllers/KorisnikController.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TextITMVC.Controllers
+{
+    public class KorisnikController : Controller
+    {
+
+
+
+        HttpClient client;
+        //The URL of the WEB API Service
+        string url = "http://localhost:3106/api/korisnik";
+
+        //The HttpClient Class, this will be used for performing 
+        //HTTP Operations, GET, POST, PUT, DELETE
+        //Set the base address and the Header Formatter
+        public KorisnikController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: EmployeeInfo
+        public async Task<ActionResult> Index()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Employees = JsonConvert.DeserializeObject<List<Korisnik>>(responseData);
+
+                return View(Employees);
+            }
+            return View("Error");
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Korisnik());
+        }
+
+        //The Post method
+        [HttpPost]
+        public async Task<ActionResult> Create(Korisnik Emp)
+        {
+
+            HttpResponseMessage responseMessage = await client.PostAsJsonAsync(url, Emp);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Employee = JsonConvert.DeserializeObject<Korisnik>(responseData);
+
+                return View(Employee);
+            }
+            return View("Error");
+        }
+
+        //The PUT Method
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, Korisnik Emp)
+        {
+
+            HttpResponseMessage responseMessage = await client.PutAsJsonAsync(url + "/" + id, Emp);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Employee = JsonConvert.DeserializeObject<Korisnik>(responseData);
+
+                return View(Employee);
+            }
+            return View("Error");
+        }
+
+        //The DELETE method
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id, Korisnik Emp)
+        {
+
+            HttpResponseMessage responseMessage = await client.DeleteAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+
+
+
+
+    }
+}
