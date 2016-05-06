f4906a8ff2aca799ecf1309f661490e7127c9111
Larisa Besic <lbesic1@etf.unsa.ba>
2016-05-06 20:45:15 +0200

    Nedovrsen prevod

---
+++ NWTServis/TextITMVC/Scripts/MultiLan.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('app', "pascalprecht.translate");
+app.controller("appctrl", ["$scope", "$translate", function ($scope, $translate) {
+    $scope.selectedLanguage = $translate.prefferedLanguage;
+    $scope.switchLanguage = function (lang) {
+        $translate.use(lang);
+    }
+}]);
+app.config(["$translateProvider", function ($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'Languages/lang-',
+        suffix: '.json'
+    });
+    $translateProvider.prefferedLanguage('bs');
+}])
