b40eead14b63475dabcc0cf74c51a9c25b348bfb
Larisa Besic <lbesic1@etf.unsa.ba>
2016-05-08 14:25:41 +0200

    Zavrsen prevod

--- NWTServis/TextITMVC/Scripts/MultiLan.js
+++ NWTServis/TextITMVC/Scripts/MultiLan.js
@@ -1,14 +1,20 @@
-﻿var app = angular.module('app', "pascalprecht.translate");
+﻿var app = angular.module('app', ['pascalprecht.translate']);
 app.controller("appctrl", ["$scope", "$translate", function ($scope, $translate) {
-    $scope.selectedLanguage = $translate.prefferedLanguage;
+    $scope.selectedLanguage = $translate.preferredLanguage;
     $scope.switchLanguage = function (lang) {
         $translate.use(lang);
     }
 }]);
 app.config(["$translateProvider", function ($translateProvider) {
-    $translateProvider.useStaticFilesLoader({
-        prefix: 'Languages/lang-',
-        suffix: '.json'
+    $translateProvider.translations('en', {
+        KORISNICI: 'Users',
+        TEKSTOVI: 'Texts',
+        VISE: "More"
+    })
+    $translateProvider.translations('bs', {
+        KORISNICI: 'Korisnici',
+        TEKSTOVI: 'Tekstovi',
+        VISE: "Više"
     });
-    $translateProvider.prefferedLanguage('bs');
+    $translateProvider.preferredLanguage('bs');
 }])
