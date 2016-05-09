618fca426465cfe3c8cb3ce8a0d87dfcb8d17024
sgrosic1 <sgrosic1@etf.unsa.ba>
2016-05-09 13:31:28 +0200

    GoogleRecaptcha

--- NWTServis/TextITMVC/Controllers/RegistrationController.cs
+++ NWTServis/TextITMVC/Controllers/RegistrationController.cs
@@ -7,12 +7,16 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using reCAPTCHA;
+using reCAPTCHA.MVC;
+using System.Net; 
+
 
 namespace TextITMVC.Controllers
 {
     public class RegistrationController : Controller
     {
-        
+
          HttpClient client;
          string url = "http://localhost:3106/api/korisnik";
 
@@ -44,26 +48,142 @@ namespace TextITMVC.Controllers
             return View(new Korisnik());
         }
 
+        
+
         [HttpPost]
         public async Task<ActionResult> RegisterUser(Korisnik Emp)
         {
 
-            HttpResponseMessage responseMessage = await client.PostAsJsonAsync<Korisnik>(url, Emp);
+           // HttpResponseMessage responseMessage = await client.PostAsJsonAsync<Korisnik>(url, Emp);
         
-           // responseMessage.EnsureSuccessStatusCode();
-            //responseMessage.Content.ReadAsAsync<HttpResponseMessage>().Wait();
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                
-                return RedirectToAction("Index", "Home");
-            }
-            return RedirectToAction("Error");
+           
+               //dodan za recaptcha
+                var response = Request["g-recaptcha-response"];
+                //secret that was generated in key value pair
+                const string secret = "6LcRYx8TAAAAAGlbk0C8WvSBIFjpzZ6j1pTIQCuC";
+
+                var client1 = new WebClient();
+                var reply =
+                    client1.DownloadString(
+                        string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, response));
+
+                var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(reply);
+
+                //when response is false check for the error message
+                if (!captchaResponse.Success)
+                {
+                    if (captchaResponse.ErrorCodes.Count <= 0) return View();
+
+                    var error = captchaResponse.ErrorCodes[0].ToLower();
+                    switch (error)
+                    {
+                        case ("missing-input-secret"):
+                            ViewBag.Message = "The secret parameter is missing.";
+                            TempData["notice"] = "The secret parameter is missing.";
+                            return RedirectToAction("RegisterUser", "Registration");
+                           // break;
+                        case ("invalid-input-secret"):
+                            ViewBag.Message = "The secret parameter is invalid or malformed.";
+                            TempData["notice"] = "The secret parameter is invalid or malformed.";
+                            return RedirectToAction("RegisterUser", "Registration");
+                           // break;
+
+                        case ("missing-input-response"):
+                            ViewBag.Message = "The response parameter is missing.";
+                            TempData["notice"] = "The response parameter is missing.";
+                            return RedirectToAction("RegisterUser", "Registration");
+                           // break;
+                        case ("invalid-input-response"):
+                            ViewBag.Message = "The response parameter is invalid or malformed.";
+                            TempData["notice"] = "The secret parameter is invalid or malformed.";
+                            return RedirectToAction("RegisterUser", "Registration");
+                           // break;
+
+                        default:
+                            ViewBag.Message = "Error occured. Please try again";
+                            TempData["notice"] = "Error occured. Please try again";
+                            return RedirectToAction("RegisterUser", "Registration");
+                            //break;
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Valid";
+                    HttpResponseMessage responseMessage = await client.PostAsJsonAsync<Korisnik>(url, Emp);
+                    if (responseMessage.IsSuccessStatusCode)
+                    {
+                        TempData["alertMessage"] = "USPJESNO";
+                    }
+                    else
+                        return RedirectToAction("Error");
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                //kraj
+
+
+            // if (responseMessage.IsSuccessStatusCode)
+            // {
+                //return RedirectToAction("Index", "Home");
+            //}
+            //return RedirectToAction("Error");
         }
 
 
         // GET: /Registration/
         /*public ActionResult Index()
         {
+            return View();
+        }*/
+
+
+      /*  [HttpPost]
+        public ActionResult ValidateCaptcha()
+        {
+            var response = Request["g-recaptcha-response"];
+            //secret that was generated in key value pair
+            const string secret = "6LcRYx8TAAAAAGlbk0C8WvSBIFjpzZ6j1pTIQCuC";
+
+            var client = new WebClient();
+            var reply =
+                client.DownloadString(
+                    string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, response));
+
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(reply);
+
+            //when response is false check for the error message
+            if (!captchaResponse.Success)
+            {
+                if (captchaResponse.ErrorCodes.Count <= 0) return View();
+
+                var error = captchaResponse.ErrorCodes[0].ToLower();
+                switch (error)
+                {
+                    case ("missing-input-secret"):
+                        ViewBag.Message = "The secret parameter is missing.";
+                        break;
+                    case ("invalid-input-secret"):
+                        ViewBag.Message = "The secret parameter is invalid or malformed.";
+                        break;
+
+                    case ("missing-input-response"):
+                        ViewBag.Message = "The response parameter is missing.";
+                        break;
+                    case ("invalid-input-response"):
+                        ViewBag.Message = "The response parameter is invalid or malformed.";
+                        break;
+
+                    default:
+                        ViewBag.Message = "Error occured. Please try again";
+                        break;
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Valid";
+            }
+
             return View();
         }*/
 	}
