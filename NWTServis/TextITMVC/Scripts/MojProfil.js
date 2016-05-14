b99ac7ab7feeeb7014b461bcf552e3731912ae47
Dzenana Bricic <dzenanabricic@hotmail.com>
2016-05-14 21:20:54 +0200

    Izmjene na moj profil

--- NWTServis/TextITMVC/Scripts/MojProfil.js
+++ NWTServis/TextITMVC/Scripts/MojProfil.js
@@ -3,15 +3,27 @@
 app.controller('MojProfil', ["$scope", "$http", function ($scope, $http) {
     $scope.mojProfil = function (k) {
         $scope.korisnik = {
-            id: sessionStorage.getItem("ID"),
-            ime: sessionStorage.getItem("ime"),
-            prezime: sessionStorage.getItem("prezime"),
-            korisnickoIme: sessionStorage.getItem("korisnickoIme"),
-            email: sessionStorage.getItem("email"),
-            tipKorisnika: sessionStorage.getItem("tipKorisnika"),
-            potvrda: sessionStorage.getItem("potvrda"),
-            sifra: sessionStorage.getItem("sifra")
+            ime: '',
+            prezime: '',
+            korisnickoIme: '',
+            email: '',
+            tipKorisnika: '',
+            potvrda: ''
         }
+        $http.get("http://localhost:3106/api/Korisnik/" + sessionStorage.getItem("ID")).success(function (response) {
+            var arr = $.map(response, function (el) { return el });
+            $scope.korisnik.id = arr[0];
+            $scope.korisnik.ime = arr[1];
+            $scope.korisnik.prezime = arr[2];
+            $scope.korisnik.korisnickoIme = arr[3];
+            $scope.korisnik.sifra = arr[4];
+            $scope.korisnik.email = arr[5];
+            $scope.korisnik.tipKorisnika = arr[6];
+            $scope.korisnik.potvrda = arr[7];
+        }).error(function (data, status) {
+            alert("Neuspjesno!");
+        });
+       
     }
 
     $scope.updateKorisnik = function (k) {
@@ -23,7 +35,7 @@ app.controller('MojProfil', ["$scope", "$http", function ($scope, $http) {
             email: $scope.korisnik.email,
             tipKorisnika: $scope.korisnik.tipKorisnika,
             sifra: $scope.korisnik.sifra,
-            potvrda: $scope.korisnik.potvrda
+            potvrda: sessionStorage.getItem("potvrda")
            
         }
         $http.put("http://localhost:3106/api/Korisnik/" + $scope.korisnik.id, $scope.korisnik).success(function (response) {
