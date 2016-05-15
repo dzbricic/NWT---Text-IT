135b69df961c4c840824bbe303a7379f8ef81302
Dzenana Bricic <dzenanabricic@hotmail.com>
2016-05-15 19:16:24 +0200

    Pocetna - tekstovi - angular

---
+++ NWTServis/TextITMVC/Scripts/Home.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('app');
+
+app.controller('Tekst', ["$scope", "$http", function ($scope, $http) {
+    $scope.tekstovi = []
+    $scope.getTekstovi= function () {
+        $http.get("http://localhost:3106/api/Tekst", $scope.korisnici).success(function (data, status) {
+            $scope.tekstovi = data;
+
+        }).error(function (data, status) {
+            alert("Neuspjesno!");
+        });
+    }
+
+}]);
