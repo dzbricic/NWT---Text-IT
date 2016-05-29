eb9df4bf8e56329af1aa549e1d185c502b0619c1
sgrosic1 <sgrosic1@etf.unsa.ba>
2016-05-29 12:38:19 +0200

    Reset pass

--- NWTServis/TextITMVC/Controllers/KorisnikController.cs
+++ NWTServis/TextITMVC/Controllers/KorisnikController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +15,7 @@ namespace TextITMVC.Controllers
     public class KorisnikController : Controller
     {
 
-        
+
 
         HttpClient client;
         //The URL of the WEB API Service
@@ -46,9 +47,6 @@ namespace TextITMVC.Controllers
             return View("Error");
         }
 
-
-
-       
 
         public ActionResult Create()
         {
@@ -127,6 +125,90 @@ namespace TextITMVC.Controllers
             return View();
         }
 
+        //funkcija za slanje maila za reset pass
+        private void SendRegistrationMail(string mailTo)
+        {
+            string link = "http://localhost:36729/Korisnik/confirmregistration"; // izmijeniti link
+
+            MailMessage msg = new MailMessage();
+
+            msg.From = new MailAddress("sgrosic1@gmail.com");
+            msg.To.Add(new MailAddress(mailTo));
+            msg.Body = "Za izmjenu šifre kliknite na sljedeći <a href=" + link + ">link.</a> ";
+            msg.Subject = "Izmjena šifre";
+            msg.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", Convert.ToInt32(587));
+            smtpClient.Credentials = new System.Net.NetworkCredential("sgrosic1", "88aida19");
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(msg);
+        }
+
+        //akcija za slanje korisnika na novi View kada klikne link za potvrdu izmjene lozinke
+        public async Task<ActionResult> confirmregistration()
+        {
+           
+               return View("~/Views/Korisnik/Izmjena.cshtml");         
+
+        }
+
+        //akcija za izmjenu nove lozinke 
+        public async Task<ActionResult> IzmijeniLozinku(FormCollection collection)
+        {
+            int userId = Convert.ToInt32(Session["id"].ToString());
+            string nova = collection.Get("sifra").ToString();
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + userId);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                var korisnik = JsonConvert.DeserializeObject<Korisnik>(responseData);
+                if (korisnik != null)
+                {
+                    korisnik.sifra = nova; 
+                    HttpResponseMessage responseMessage1 = await client.PutAsJsonAsync(url + "/" + userId, korisnik);
+                    if (responseMessage1.IsSuccessStatusCode)
+                    {
+                        return View("~/Views/Korisnik/Uspjesno.cshtml");
+                    }
+                }
+                else return View("~/Views/Korisnik/Neuspjesno.cshtml");
+
+            }
+            return View("Error");
+        }
+
+
+
+        public ActionResult Izmjena()
+        {
+            return View(new Korisnik());
+        }
+
+        public ActionResult Reset()
+        {
+            return View(new Korisnik());
+        }
+
+        //akcija za pronalazak korisnika putem mail-a, te pozivanje funkcije za slanje mail-a
+        public async Task<ActionResult> ResetAction(FormCollection collection)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string email = collection.Get("email").ToString(); 
+                //var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                List<Korisnik> l = await responseMessage.Content.ReadAsAsync<List<Korisnik>>();
+                //var Employees = JsonConvert.DeserializeObject<List<Korisnik>>(responseData); // lista korisnika
+                Korisnik m = l.Find(k => k.email.ToString() == email); // pronalazi korisnika s mail-om koji smo mu proslijedili
+                Session["id"] = m.korisnikID;
+                SendRegistrationMail(email); // šalje mu mail
+
+                return View("~/Views/Korisnik/Provjeri.cshtml");
+            }
+            return View("Error");
+
+        }
+        
 
 
 
